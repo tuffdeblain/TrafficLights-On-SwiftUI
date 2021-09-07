2790b3f28d557d0eb14ef33ad514a469f7a6c962
tuffdeblain <himanfullhd@gmail.com>
2021-09-07 20:22:06 +0700

    Initial Commit

---
+++ TrafficLights On SwiftUI/TrafficLights_On_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLights_On_SwiftUIApp.swift
+//  TrafficLights On SwiftUI
+//
+//  Created by Сергей Кудинов on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLights_On_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
