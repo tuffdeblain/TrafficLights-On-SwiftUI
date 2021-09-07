df72b49ed7abd2a34f089ed15e939dd0da1e8231
tuffdeblain <himanfullhd@gmail.com>
2021-09-07 23:15:54 +0700

    Second variation

--- TrafficLights On SwiftUI/ContentView.swift
+++ TrafficLights On SwiftUI/ContentView.swift
@@ -16,9 +16,9 @@ struct ContentView: View {
     
     @State var currentLight = CurrentLight.red
     @State var buttonText = "Start"
-    @State var redSatus = 0.3
-    @State var yellowStatus = 0.3
-    @State var greenStatus = 0.3
+    @State var redSatus = 0.4
+    @State var yellowStatus = 0.4
+    @State var greenStatus = 0.4
     let lightIsOn = 1.0
     let lightIsOff = 0.4
     
@@ -73,45 +73,6 @@ struct ContentView: View {
         }
     }
 }
-
-//private var tapButton: some View {
-//
-//    Button(action: {
-//        print("salam")
-//        if  buttonText  == "Start" {
-//            buttonText = "Next"
-//        }
-//
-//        switch currentLight {
-//        case .red:
-//            redSatus = lightIsOn
-//            greenStatus = lightIsOff
-//            currentLight = .yellow
-//        case .yellow:
-//            yellowStatus = lightIsOn
-//            redSatus = lightIsOff
-//            currentLight = .green
-//        case .green:
-//            greenStatus = lightIsOn
-//            yellowStatus = lightIsOff
-//            currentLight = .red
-//        }
-//
-//    }, label: {
-//        Text(buttonText)
-//            .bold()
-//            .foregroundColor(.white)
-//            .frame(width: 150, height: 50)
-//            .background(Color.blue)
-//
-//            .overlay(
-//                RoundedRectangle(cornerRadius: 230)
-//                    .stroke(Color.white, lineWidth: 5)
-//            )
-//            .clipShape(Capsule())
-//
-//    })
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
